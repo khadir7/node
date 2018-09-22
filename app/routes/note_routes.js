48b553274430b5d7edf3b6604e445aa0ec460854
khadir7 <khadirahmed.sd@gmail.com>
2018-09-22 18:27:53 +0530

    CRUD changes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,68 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db){
     app.post('/notes', (req, res) => {
+
         // You'll create your note here.
-        res.send('Hello')
+        // console.log("note collection:::::", db.collection('notes'))
+        console.log("request object::::", req.body)
+        const note = {
+            title: req.body.title,
+            data: req.body.data
+        }
+        db.collection('notes').insert(note, (err, results) => {
+            if(err){
+                res.send({err: "An error has occured"});
+            }else{
+                res.send(results.ops[0])
+            }
+        })
+        // res.send('Hello')
+    })
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            _id: new ObjectID(id)
+        }
+        db.collection('notes').findOne(details, (err, results) => {
+            if(err){
+                res.send({err:"An error has occured"})
+            }else{
+                res.send(results)
+            }
+        })
+    })
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            _id: new ObjectID(id)
+        }
+        db.collection('notes').remove(details, (err, results) => {
+            if(err){
+                res.send({err:"An error has occured"})
+            }else{
+                res.send("This item("+id+") is deleted")
+            }
+        })
+    })
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            _id: new ObjectID(id)
+        }
+        const note = {
+            title: req.body.title,
+            data: req.body.data
+        }
+        db.collection('notes').update(details, note, (err, results) => {
+            if(err){
+                res.send({err:"An error has occured"})
+            }else{
+                res.send(note)
+            }
+        })
     })
 }
--- server.js
+++ server.js
@@ -2,11 +2,20 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-const routes         = require('./app/routes')
+const routes         = require('./app/routes');
+const db             = require('./config/db');
 const app            = express();
 
 const port = 8000;
-routes(app, {})
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database) => {
+  if(err){
+    return console.log(err);
+  }
+  routes(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
